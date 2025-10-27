e75b8f57e42b5197401263ec08e69ac7e122c84d
Kletterzentrum <kasse1@kletterzentrum-tirschenreuth.de>
2025-10-27 10:46:24 +0100

    Update update-mitglieder.js

--- scripts/update-mitglieder.js
+++ scripts/update-mitglieder.js
@@ -1,6 +1,15 @@
 // scripts/update-mitglieder.js
 import fs from "fs";
 
+async function loadPage(){
+  const response = await fetch("https://dav-karlsbad.de");
+  const htmlContent = await response.text();
+  const page = htmlContent;
+}
+
+loadPage();
+
+/*
 async function updateMitglieder() {
   try {
     const url = "https://dav-karlsbad.de";
@@ -17,6 +26,17 @@ async function updateMitglieder() {
       stand: new Date().toISOString(),
       source: url
     };
+*/
+
+document.body.innerHTML = page;
+const element = document.getElementsByClassName('percent-value')[0];
+const text = element.innerText;
+
+    const data = {
+      mitglieder: mitglieder ?? text,
+      stand: new Date().toISOString(),
+      source: url
+    };
 
     // Stelle sicher, dass data-Ordner existiert
     if (!fs.existsSync("data")) fs.mkdirSync("data");
@@ -27,7 +47,7 @@ async function updateMitglieder() {
     console.error("Fehler beim Aktualisieren der Mitgliedszahl:", err);
     // Schreibe Fehlerstatus in JSON, damit Frontend nicht komplett scheitert
     const data = {
-      mitglieder: "1716",
+      mitglieder: "XXX",
       stand: new Date().toISOString(),
       error: String(err)
     };
